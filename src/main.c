816572dbac06957473dd85b25b39c9d7a2876bc1
Jan Horacek <me@apophis.cz>
2021-03-09 11:44:56 +0100

    Add testing program.

--- src/main.c
+++ src/main.c
@@ -14,9 +14,22 @@ void init();
 
 int main() {
 	init();
-	while (true) {}
+
+	set_output(7, true); // DEBUG
+
+	while (true) {
+		set_power(0, true);
+		_delay_us(50);
+		set_power(0, false);
+		_delay_ms(1);
+	}
 }
 
 void init() {
+	// Enable outputs
+	DDRB = 0xFF;
+	DDRC = 0x03;
+	DDRD = 0xF0;
+
 	sei();
 }
